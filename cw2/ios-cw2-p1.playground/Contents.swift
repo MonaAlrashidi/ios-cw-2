eb545044fd35d2c02b54c9a901a0c2960f1944f6
Mona Alrashidi <63103306+MonaAlrashidi@users.noreply.github.com>
2020-06-17 20:54:18 +0300

    Classwork 2
    cw2 folder has p123 inside

---
+++ cw2/ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,15 @@
+let family = ["Mona", "Omaima", "Basheer", "Rashid", "Dalal"]
+
+var evens = [0, 2, 4, 6, 8]
+
+var grades = [99.1, 98.3, 98.6]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[4])
+
+print(evens[0], evens[4])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
